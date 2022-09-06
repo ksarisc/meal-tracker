32dac2a1005bc13ec75d885a0bbf23b4a42cfce0
ksarisc <jacobsmith.in@gmail.com>
2022-09-05 22:50:52 -0400

    fix model string defaults

--- MealTracker/Models/MealItem.cs
+++ MealTracker/Models/MealItem.cs
@@ -6,11 +6,14 @@ namespace MealTracker.Models
     {
         public int Id { get; set; }
         [Required]
-        public string Description { get; set; }
+        [StringLength(1000, MinimumLength = 2)]
+        public string Description { get; set; } = string.Empty;
 
         public int Calories { get; set; }
 
         public DateTimeOffset CreatedAt { get; set; }
-        public string CreatedBy { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 4)]
+        public string CreatedBy { get; set; } = string.Empty;
     }
 }
